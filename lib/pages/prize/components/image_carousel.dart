cceefca6ca5d35b17a01f464e0fc2ce8dfaef1b3
nck.974 <nck.974@gmail.com>
2023-08-30 18:37:44 +0200

    Display total images and current image in the carousel of images

--- lib/pages/prize/components/image_carousel.dart
+++ lib/pages/prize/components/image_carousel.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ImageCarousel extends StatelessWidget {
+class ImageCarousel extends StatefulWidget {
   final List<String> imageUrls;
 
   const ImageCarousel({super.key, required this.imageUrls});
 
   @override
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.toInt();
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: imageUrls.length,
-      itemBuilder: (context, index) {
-        return Center(
-          child: InteractiveViewer(
-            boundaryMargin:
-                const EdgeInsets.all(20.0), // Margin around the image
-            minScale: 0.1, // Minimum scale to allow zoom out
-            maxScale: 2.0, // Maximum scale to allow zoom in
-            child: Image.asset(
-              imageUrls[index],
-              fit: BoxFit.contain, // Fit the image to the screen width
-            ),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: widget.imageUrls.length,
+            itemBuilder: (context, index) {
+              return Center(
+                child: InteractiveViewer(
+                  boundaryMargin: const EdgeInsets.all(20.0),
+                  minScale: 0.1, // Minimum scale to allow zoom out
+                  maxScale: 3.0, // Maximum scale to allow zoom in
+                  child: Image.asset(
+                    widget.imageUrls[index],
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              );
+            },
           ),
-        );
-      },
+        ),
+        Container(
+          margin: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '${_currentPage + 1}', // Current image index
+                style: const TextStyle(fontSize: 18),
+              ),
+              Text(
+                ' / ${widget.imageUrls.length}', // Total number of images
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 }
